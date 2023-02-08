45853246403da00f0aca1e2638f6a2f9d6b57c3a
Shivanshu04 <shivu2jaun@gmail.com>
2023-02-08 16:08:46 +0530

    add conditions

--- GreatestNumber.java
+++ GreatestNumber.java
@@ -1,3 +1,7 @@
+// wap to find greater among than three number
+import java.util.Scanner;
 public class GreatestNumber {
-    
+    int a;
+    public static void main(String args[]);
+    Scanner Sc = new Scanner(System.in)
 }
