2b312362073a7ef6f64e5b5cf2609e55d1a5b020
Shivanshu04 <shivu2jaun@gmail.com>
2023-02-08 16:12:41 +0530

    added condition

--- GreatestNumber.java
+++ GreatestNumber.java
@@ -1,7 +1,41 @@
 // wap to find greater among than three number
 import java.util.Scanner;
 public class GreatestNumber {
-    int a;
-    public static void main(String args[]);
-    Scanner Sc = new Scanner(System.in)
+   
+
+    public static void main(String args[]){
+        int a,b,c;
+    
+        Scanner Sc = new Scanner(System.in);
+        System.out.println("Enter any three number");
+        a=Sc.nextInt();
+        b=Sc.nextInt();
+        c=Sc.nextInt();
+        if(a>b)
+        {
+            if(a>c)
+            {
+                System.out.println("a is grater");
+            }
+            else
+            {
+                System.out.println("c is greater");
+            }
+        }
+        else
+        {
+            if(b>c) 
+            {
+                System.out.println("b is greater");
+            }
+            else
+            {
+                System.out.println("c is greater");
+
+            }
+    }
+    
+
+   
+}
 }
