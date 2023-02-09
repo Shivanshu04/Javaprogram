0b08b2cf453e6789e1c67c8b50184b678954a07b
Shivanshu04 <shivu2jaun@gmail.com>
2023-02-09 21:42:00 +0530

    create a pattern program

---
+++ Pattern.java
@@ -0,0 +1,13 @@
+public class Pattern {
+    public static void main(String[] args) {
+        int i,j;
+        for(i=1;i<=8;i++)
+        {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
