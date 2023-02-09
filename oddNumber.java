b68a1fca0f8a76e7bbbfd5f7f881064859abb052
Shivanshu04 <shivu2jaun@gmail.com>
2023-02-09 21:24:17 +0530

    odd number fumction

--- oddNumber.java
+++ oddNumber.java
@@ -1,4 +1,12 @@
 //wap to print 1 to 100 only odd number
 public class oddNumber {
-    
+    public static void main(String[] args) {
+        int a;
+        Scanner sc =  new Scanner(System.in);
+        System.out.print("Enter your Number");
+        a= sc.nextInt();
+        for(int i=1;i<=100;i++){
+            System.out.println(i*a);
+        }
+    }
 }
