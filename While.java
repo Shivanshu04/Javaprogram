4ccf5585041c4c2e8963ac9b823d688450f425f8
Shivanshu04 <shivu2jaun@gmail.com>
2023-02-09 21:54:07 +0530

    create even using while

---
+++ While.java
@@ -0,0 +1,17 @@
+// wap to print all the ven 
+public class While {
+   
+	public static void main(String[] args)  
+	{
+		int number = 1;
+ 
+		while( number <= 100 ) 
+		{ 
+			if( (number % 2) == 0 ) 
+			{ 
+				System.out.println(number); 
+			} 
+			number++; 
+		} 
+	} 
+} 
