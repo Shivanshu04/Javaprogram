a3d2ec0f71205eed1f22a1b6beffd8f39c1775f5
Shivanshu04 <shivu2jaun@gmail.com>
2023-02-09 16:39:11 +0530

    create table program

---
+++ Table.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+import javax.security.sasl.SaslException;
+
+public class Table {
+    public static void main(String[] args) {
+        int a;
+        Scanner sc =  new Scanner(System.in);
+        System.out.print("Enter your Number");
+        a= sc.nextInt();
+        for(int i=1;i<=10;i++){
+            System.out.println(i*a);
+        }
+    }
+}
