299e7c828032d08b5ce70527c5eaa6fd791e60d9
Shivanshu04 <shivu2jaun@gmail.com>
2023-02-20 19:33:25 +0530

    default constructir

---
+++ default.java
@@ -0,0 +1,19 @@
+import javax.sound.sampled.SourceDataLine;
+
+// default constructor
+class Simple {
+    int a;
+    boolean b;
+Simple()
+{
+    a=0;
+    b= false;
+}
+System.out.println(String[] args);
+    {
+        Simple S = new Simple();
+        System.out.println(S.a);
+        System.out.println(S.b);
+    }
+
+}
