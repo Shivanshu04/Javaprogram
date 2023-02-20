7f6f85a1fe33b0c26ec1307652c96e826ec4a5d2
Shivanshu04 <shivu2jaun@gmail.com>
2023-02-20 18:24:20 +0530

    using do while condition

---
+++ dowhile.java
@@ -0,0 +1,13 @@
+public class dowhile {
+    public static void main(String[] args) {
+        int a=1;
+        do
+        {
+            System.out.println("odd =" +a);
+            a+=2;
+        }while(a<=51);
+
+        }
+    }
+    
+
