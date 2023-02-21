4803395463d8c215acb8c4d32b4dff8829d972cc
Shivanshu04 <shivu2jaun@gmail.com>
2023-02-21 16:35:23 +0530

    new

---
+++ overloading.java
@@ -0,0 +1,9 @@
+ class overloading {
+    void Add(int x , int y)
+    int z = x+y;
+    System.out.println("Sum="+z);
+    {void Add(int x , int y,int z)
+    int r = x+y+z;
+    System.out.println("Sum="+r);
+    }
+}
