3f2ffbe21a39dbb905ea5dd19be52616a4d07945
Shivanshu04 <shivu2jaun@gmail.com>
2023-02-24 22:19:12 +0530

    primitive program

---
+++ primitive.java
@@ -0,0 +1,24 @@
+import javax.xml.transform.Source;
+
+public class primitive {
+    public static void main(String[] args) {
+        int rollno = 64;
+        //primitive data types are those datatype which do not split into sub group
+        // int  is a primitive datatype
+        //string is not primitive datatype
+        char letter = 'r';
+        float marks = 40.56f;
+        double large_decimal_values =33476543.7899;
+        long largeinteger = 3459876543L;
+        boolean check = true;
+
+        System.out.println(rollno);
+        System.out.println(letter);
+        System.out.println(marks);
+        System.out.println(large_decimal_values);
+        System.out.println(largeinteger);
+        System.out.println(check);
+
+    }
+}
+ 
