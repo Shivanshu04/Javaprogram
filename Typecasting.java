ce75f51436f489407713a3216fdb275e7b735d0e
Shivanshu04 <shivu2jaun@gmail.com>
2023-02-25 01:35:20 +0530

    typecasting prog

---
+++ Typecasting.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Typecasting {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        float num = input.nextFloat();
+        System.out.println(num);
+        // type casting change float into int
+        int sum = (int)(67.87f);
+        System.out.println(sum);
+
+
+    }    
+}
