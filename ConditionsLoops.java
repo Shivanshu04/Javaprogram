16831520c57b290038cde2af89de488f4b1ee47a
Shivanshu04 <shivu2jaun@gmail.com>
2023-02-26 21:54:31 +0530

    not changed

--- ConditionsLoops.java
+++ ConditionsLoops.java
@@ -1,8 +1,8 @@
 public class ConditionsLoops {
     public static void main(String[] args) {
         //print 1 t0 5
-        //for(int num=1; num<=5;num +=1){
-           // System.out.println(num);
+        for(int num=1; num<=5;num +=1){
+           System.out.println(num);
         }
     }
 }
