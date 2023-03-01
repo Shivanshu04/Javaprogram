518cb2b034f871b5d19790729ef05ec4b2a7b288
Shivanshu04 <shivu2jaun@gmail.com>
2023-03-01 22:02:50 +0530

    array

---
+++ array.java
@@ -0,0 +1,20 @@
+public class array {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        arr[0]=34;
+        arr[1]=4;
+        arr[2]=347;
+        arr[3]=345;
+        arr[4]=343;
+        // System.out.println(arr[3]);
+
+        // input using for loop
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]+" ");
+
+            for (int j = 0; j < arr.length; j++) {
+                
+            }
+        }
+    }
+}
