be3e831fd45b3cf1f12ba9edc0292a97287e8b7a
Shivanshu04 <shivu2jaun@gmail.com>
2023-03-01 23:18:00 +0530

    hii

---
+++ array_2d.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class array_2d {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int[][] arr = new int[3][3];
+        System.out.println(arr.length);
+        for (int row = 0; row < arr.length; row++) {
+            for( int col =0; col<arr.length;col++){
+                arr[row][col]=in.nextInt();
+            }
+         
+            }
+        }
+        for (int row = 0; row < arr.length; row++){
+            System.out.println(Arrays.toString(arr[row]));
+        //output
+
+
+    }
+}
