13724f5fc12cdec71ab89ed134696e4e43b78f10
Shivanshu04 <shivu2jaun@gmail.com>
2023-03-02 20:28:04 +0530

    new changes

--- array_2d.java
+++ array_2d.java
@@ -5,19 +5,25 @@ public class array_2d {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        int[][] arr = new int[3][3];
+        int[][] arr = new int[3][];
         System.out.println(arr.length);
-        for (int row = 0; row < arr.length; row++) {
-            for( int col =0; col<arr.length;col++){
-                arr[row][col]=in.nextInt();
-            }
-         
-            }
+         //for (int row = 0; row < arr.length; row++) 
+        {
+        //     for( int col =0; col<arr.length;col++){
+        //         arr[row][col]=in.nextInt();
+        //     }
+        // for (int row = 0; row < arr.length; row++){
+        //     System.out.println(Arrays.toString(arr[row]));
+        //     }
+
+        for(int[]a:arr){
+            System.out.println(Arrays.toString(arr));
         }
-        for (int row = 0; row < arr.length; row++){
-            System.out.println(Arrays.toString(arr[row]));
+
+        }
+        
         //output
 
 
     }
-}
+
