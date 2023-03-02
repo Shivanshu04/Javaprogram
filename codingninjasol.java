8f9b75b82faee0895f402a8bed7d3eff19d8f9cc
Shivanshu04 <shivu2jaun@gmail.com>
2023-03-02 19:25:40 +0530

    new

---
+++ codingninjasol.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class coadingninjasol {
+
+    public static void main(String args[]) {
+
+        // Write code here
+
+        Scanner sc=new Scanner(System.in);
+
+        String name=sc.next();
+
+        int age=sc.nextInt();
+
+        System.out.print("The name of the person is "+name+" and the age is "+age+". ");
+
+    }
+
+}
---
+++ hackerrank.java
@@ -0,0 +1,25 @@
+public class hackerrank {
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        if(n%2=1){
+            System.out.println("weird");
+        }
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        scanner.close();
+    }
+}
